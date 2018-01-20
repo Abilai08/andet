48312dc496ce5a4836c667b3b4aa4e8b2605dc2a
Abylay <abylai0808@gmail.com>
2018-01-20 14:31:15 -0800

    committed all the files

---
+++ includes/classes/Constants.php
@@ -0,0 +1,18 @@
+<?php 
+class Constants{
+
+	public static $user5to25 = "Your username must be between 5 and 25 characters";
+	public static $fName2To25 = "Your first name must be between 2 and 25 characters";
+	public static $lastName5to25 = "Your last name must be between 5 and 25 characters";
+	public static $emailInvalid = "Your email format is invalid";
+	public static $emailTaken = "This email has been already used";
+	public static $emailNotMatch = "Your emails didn't match";
+	public static $passwordOnlyNum = "Your password can only contain only numbers and letters";
+	public static $password5to30 = "Your password must be between 5 to 30 characters";
+	public static $passwordDoNoMatch = "Your passwords don't match";
+	public static $usernameTaken = "This username has already been taken";
+
+	//login errors
+	public static $loginFailed = "Username or password is incorrect";
+}
+ ?>
